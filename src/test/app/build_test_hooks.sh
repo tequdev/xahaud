3361e3f70668358776823f1c1fdff45619f9aae7
tequ <git@tequ.dev>
2025-02-13 17:52:46 +0900

    fix: make build_test_hooks.sh compatible with macOS
    - Ensure build_test_hooks.sh works on macOS environments
    - Maintain cross-platform compatibility between Linux and macOS
    
    Note: requires 'ggrep' to be installed via Homebrew on macOS

--- src/test/app/build_test_hooks.sh
+++ src/test/app/build_test_hooks.sh
@@ -11,6 +11,7 @@ namespace ripple {
 namespace test {
 std::map<std::string, std::vector<uint8_t>> wasm = {' > SetHook_wasm.h
 COUNTER="0"
+CURRENT=$(cd $(dirname $0);pwd)
 cat SetHook_test.cpp | tr '\n' '\f' | 
         grep -Po 'R"\[test\.hook\](.*?)\[test\.hook\]"' | 
         sed -E 's/R"\[test\.hook\]\(//g' | 
@@ -19,18 +20,19 @@ cat SetHook_test.cpp | tr '\n' '\f' |
         do
             echo "/* ==== WASM: $COUNTER ==== */" >> SetHook_wasm.h
             echo -n '{ R"[test.hook](' >> SetHook_wasm.h
-            cat <<< $line | sed -E 's/.{7}$//g' | tr -d '\n' | tr '\f' '\n' >> SetHook_wasm.h
+            cat <<< "$line" | sed -E 's/.{7}$//g' | tr -d '\n' | tr '\f' '\n' >> SetHook_wasm.h
             echo ')[test.hook]",' >> SetHook_wasm.h
             echo "{" >> SetHook_wasm.h
             WAT=`grep -Eo '\(module' <<< $line | wc -l`
             if [ "$WAT" -eq "0" ]
             then
-                echo '#include "api.h"' > /root/xrpld-hooks/hook/tests/hookapi/wasm/test-$COUNTER-gen.c
-                tr '\f' '\n' <<< $line >> /root/xrpld-hooks/hook/tests/hookapi/wasm/test-$COUNTER-gen.c
+                mkdir -p $CURRENT/../../../generated/hook/c
+                echo '#include "api.h"' > $CURRENT/../../../generated/hook/c/test-$COUNTER-gen.c
+                tr '\f' '\n' <<< $line >> $CURRENT/../../../generated/hook/c/test-$COUNTER-gen.c
                 DECLARED="`tr '\f' '\n' <<< $line | grep  -E '(extern|define) ' | grep -Eo '[a-z\-\_]+ *\(' | grep -v 'sizeof' | sed -E 's/[^a-z\-\_]//g' | sort | uniq`"
                 USED="`tr '\f' '\n' <<< $line | grep -vE '(extern|define) ' | grep -Eo '[a-z\-\_]+\(' | grep -v 'sizeof' | sed -E 's/[^a-z\-\_]//g' | grep -vE '^(hook|cbak)' | sort | uniq`"
                 ONCE="`echo $DECLARED $USED | tr ' ' '\n' | sort | uniq -c | grep '1 ' | sed -E 's/^ *1 //g'`"
-                FILTER="`echo $DECLARED | tr ' ' '|' | sed -E 's/|$//g'`"
+                FILTER="`echo $DECLARED | tr ' ' '|' | sed -E 's/\|$//g'`"
                 UNDECL=`echo "$ONCE" | grep -v -E "$FILTER"`
                 if [ ! -z "$UNDECL" ]
                 then
@@ -38,14 +40,14 @@ cat SetHook_test.cpp | tr '\n' '\f' |
                     echo "$line"
                     exit 1
                 fi
-                wasmcc -x c /dev/stdin -o /dev/stdout -O2 -Wl,--allow-undefined <<< `tr '\f' '\n' <<< $line` |
+                wasmcc -x c /dev/stdin -o /dev/stdout -O2 -Wl,--allow-undefined <<< "`tr '\f' '\n' <<< $line`" |
                     hook-cleaner - - 2>/dev/null |
-                    xxd -p -u -c 19 | 
-                    sed -E 's/../0x\0U,/g' | sed -E 's/^/    /g' >> SetHook_wasm.h
+                    xxd -p -u -c 10 | 
+                    sed -E 's/../ 0x&U,/g' | sed -E 's/^/    /g' >> SetHook_wasm.h
             else
-                wat2wasm - -o /dev/stdout <<< `tr '\f' '\n' <<< $(sed -E 's/.{7}$//g' <<< $line)` |
-                    xxd -p -u -c 19 | 
-                    sed -E 's/../0x\0U,/g' | sed -E 's/^/    /g' >> SetHook_wasm.h
+                wat2wasm - -o /dev/stdout <<< "`tr '\f' '\n' <<< $(sed -E 's/.{7}$//g' <<< $line)`" |
+                    xxd -p -u -c 10 | 
+                    sed -E 's/../ 0x&U,/g' | sed -E 's/^/    /g' >> SetHook_wasm.h
             fi
             if [ "$?" -gt "0" ]
             then
