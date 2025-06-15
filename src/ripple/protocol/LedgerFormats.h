beartec-jpg/FalconLedger

Introduce AMM support (XLS-30d): (#4294)
Add AMM functionality:
- InstanceCreate
- Deposit
- Withdraw
- Governance
- Auctioning
- payment engine integration

To support this functionality, add:
- New RPC method, `amm_info`, to fetch pool and LPT balances
- AMM Root Account
- trust line for each IOU AMM token
- trust line to track Liquidity Provider Tokens (LPT)
- `ltAMM` object

The `ltAMM` object tracks:
- fee votes
- auction slot bids
- AMM tokens pair
- total outstanding tokens balance
- `AMMID` to AMM `RootAccountID` mapping

Add new classes to facilitate AMM integration into the payment engine.
`BookStep` uses these classes to infer if AMM liquidity can be consumed.

The AMM formula implementation uses the new Number class added in #4192.
IOUAmount and STAmount use Number arithmetic.

Add AMM unit tests for all features.

AMM requires the following amendments:
- featureAMM
- fixUniversalNumber
- featureFlowCross

Notes:
- Current trading fee threshold is 1%
- AMM currency is generated by: 0x03 + 152 bits of sha256{cur1, cur2}
- Current max AMM Offers is 30

---------

Co-authored-by: Howard Hinnant <howard.hinnant@gmail.com>

src/ripple/protocol/LedgerFormats.h
@@ -161,6 +161,12 @@ enum LedgerEntryType : std::uint16_t
      */
     ltNFTOKEN_OFFER = 0x0037,
 
+    /** The ledger object which tracks the AMM.
+
+       \sa keylet::amm
+    */
+    ltAMM = 0x0079,
+
     //---------------------------------------------------------------------------
     /** A special type, matching any ledger entry type.
 
@@ -245,7 +251,7 @@ enum LedgerSpecificFlags {
         0x20000000,               // True, reject new trustlines (only if no issued assets)
     lsfAMM [[maybe_unused]] = 0x40000000, // True, AMM account
     lsfAllowClawback =
-        0x80000000,               // True, enable clawback 
+        0x80000000,               // True, enable clawback
 
     // ltOFFER
     lsfPassive = 0x00010000,
@@ -260,6 +266,8 @@ enum LedgerSpecificFlags {
     lsfHighNoRipple = 0x00200000,
     lsfLowFreeze = 0x00400000,   // True, low side has set freeze flag
     lsfHighFreeze = 0x00800000,  // True, high side has set freeze flag
+    lsfAMMNode = 0x01000000,     // True, trust line to AMM. Used by client
+                                 // apps to identify payments via AMM.
 
     // ltSIGNER_LIST
     lsfOneOwnerCount = 0x00010000,  // True, uses only one OwnerCount

tequdev/xahaud

src/ripple/protocol/LedgerFormats.h
@@ -179,6 +179,12 @@ enum LedgerEntryType : std::uint16_t
      */
     ltUNL_REPORT = 0x0052, 
 
+    /** The ledger object which tracks the AMM.
+
+       \sa keylet::amm
+    */
+    ltAMM = 0x0079,
+
     //---------------------------------------------------------------------------
     /** A special type, matching any ledger entry type.
 
@@ -289,7 +295,7 @@ enum LedgerSpecificFlags {
         0x80000000,
     lsfAMM [[maybe_unused]] = 0x0004000, // True, AMM account
     lsfAllowClawback =
-        0x00008000,               // True, enable clawback 
+        0x00008000,               // True, enable clawback
 
     // ltOFFER
     lsfPassive = 0x00010000,
@@ -304,6 +310,8 @@ enum LedgerSpecificFlags {
     lsfHighNoRipple = 0x00200000,
     lsfLowFreeze = 0x00400000,   // True, low side has set freeze flag
     lsfHighFreeze = 0x00800000,  // True, high side has set freeze flag
+    lsfAMMNode = 0x01000000,     // True, trust line to AMM. Used by client
+                                 // apps to identify payments via AMM.
 
     // ltSIGNER_LIST
     lsfOneOwnerCount = 0x00010000,  // True, uses only one OwnerCount