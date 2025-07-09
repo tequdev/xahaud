810e15319ca7e44d58078e9020ea0282a2f6a62e
Denis Angell <dangell@transia.co>
2025-07-09 02:33:08 +0200

    Feature DeepFreeze (#536)
    ---------
    
    Co-authored-by: tequ <git@tequ.dev>

--- src/ripple/protocol/impl/Feature.cpp
+++ src/ripple/protocol/impl/Feature.cpp
@@ -476,6 +476,7 @@ REGISTER_FIX    (fix20250131,                   Supported::yes, VoteBehavior::De
 REGISTER_FEATURE(HookCanEmit,                   Supported::yes, VoteBehavior::DefaultNo);
 REGISTER_FIX    (fixRewardClaimFlags,           Supported::yes, VoteBehavior::DefaultYes);
 REGISTER_FIX    (fixProvisionalDoubleThreading, Supported::yes, VoteBehavior::DefaultYes);
+REGISTER_FEATURE(DeepFreeze,                    Supported::yes, VoteBehavior::DefaultNo);
 
 // The following amendments are obsolete, but must remain supported
 // because they could potentially get enabled.
