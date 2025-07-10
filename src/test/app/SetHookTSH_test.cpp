691a1bdcafb63008a9e944b8528f60ded155d5b6
tequ <git@tequ.dev>
2025-07-10 12:02:43 +0900

    Add AMMTsh tests

--- src/test/app/SetHookTSH_test.cpp
+++ src/test/app/SetHookTSH_test.cpp
@@ -20,6 +20,7 @@
 
 #include <test/app/Import_json.h>
 #include <test/jtx.h>
+#include <test/jtx/AMM.h>
 #include <test/jtx/TestHelpers.h>
 #include <xrpld/app/misc/HashRouter.h>
 #include <xrpld/app/misc/TxQ.h>
@@ -838,6 +839,391 @@ private:
 
             // verify tsh hook triggered
             testTSHStrongWeak(env, tshSTRONG, __LINE__);
+        }
+    }
+
+    // clang-format off
+    // AMM
+    // | otxn | tsh | Bid | Create | Delete | Clawback | Deposit | Vote | Withdraw |
+    // |   A  |  I  |  -  |    W   |    W   |     W    |    W    |   -  |     W    |
+    // |   A  |  H  |  -  |    -   |    -   |     W    |    -    |   -  |     -    |
+    // clang-format on
+    void
+    testAMMBidTSH(FeatureBitset features)
+    {
+        using namespace test::jtx;
+        using namespace std::literals;
+        testcase("amm bid tsh");
+
+        // otxn: account
+        // tsh issuer
+        // w/s: none
+        for (bool const testStrong : {true, false})
+        {
+            test::jtx::Env env{
+                *this,
+                network::makeNetworkConfig(21337, "10", "1000000", "200000"),
+                features};
+
+            auto const issuer = Account("gw");
+            auto const account = Account("alice");
+            auto const USD = issuer["USD"];
+
+            env.fund(XRP(30'000), issuer, account);
+            env.close();
+            env.trust(USD(30'000), account);
+            env.close();
+            env(pay(issuer, account, USD(10'000)));
+            env.close();
+
+            // create AMM
+            AMM ammAlice(env, account, XRP(10'000), USD(10'000));
+
+            // set tsh collect
+            if (!testStrong)
+                addWeakTSH(env, issuer);
+
+            // set tsh hook
+            setTSHHook(env, issuer, testStrong);
+
+            // bid
+            ammAlice.bid({
+                .account = account,
+                .bidMin = 100,
+            });
+
+            // verify tsh hook triggered
+            testTSHStrongWeak(env, tshNONE, __LINE__);
+        }
+    }
+
+    // AMMCreate
+    void
+    testAMMCreateTSH(FeatureBitset features)
+    {
+        using namespace test::jtx;
+        using namespace std::literals;
+        testcase("amm create tsh");
+
+        // otxn: account
+        // tsh issuer
+        // w/s: weak
+        for (bool const testStrong : {true, false})
+        {
+            test::jtx::Env env{
+                *this,
+                network::makeNetworkConfig(21337, "10", "1000000", "200000"),
+                features};
+
+            auto const issuer = Account("gw");
+            auto const account = Account("alice");
+            auto const USD = issuer["USD"];
+
+            env.fund(XRP(30'000), issuer, account);
+            env.close();
+            env.trust(USD(30'000), account);
+            env.close();
+            env(pay(issuer, account, USD(10'000)));
+            env.close();
+
+            // set tsh collect
+            if (!testStrong)
+                addWeakTSH(env, issuer);
+
+            // set tsh hook
+            setTSHHook(env, issuer, testStrong);
+
+            // create AMM
+            AMM ammAlice(env, account, XRP(10'000), USD(10'000));
+
+            // verify tsh hook triggered
+            if (features[featureIOUIssuerWeakTSH])
+            {
+                auto const expected = testStrong ? tshNONE : tshWEAK;
+                testTSHStrongWeak(env, expected, __LINE__);
+            }
+            else
+            {
+                testTSHStrongWeak(env, tshNONE, __LINE__);
+            }
+        }
+    }
+
+    // AMMDelete
+    void
+    testAMMDeleteTSH(FeatureBitset features)
+    {
+        using namespace test::jtx;
+        using namespace std::literals;
+        testcase("amm delete tsh");
+
+        // otxn: account
+        // tsh issuer, holder
+        // w/s: none
+        for (bool const testStrong : {true, false})
+        {
+            test::jtx::Env env(
+                *this,
+                envconfig([](std::unique_ptr<Config> cfg) {
+                    cfg->FEES.reference_fee = XRPAmount(1);
+                    return cfg;
+                }),
+                features);
+
+            auto const issuer = Account("gw");
+            auto const account = Account("alice");
+            auto const bob = Account("bob");
+            auto const USD = issuer["USD"];
+
+            env.fund(XRP(20'000), issuer, account, bob);
+            env.close();
+            env.trust(USD(10'000), account);
+            env.close();
+            env(pay(issuer, account, USD(10'000)));
+            env.close();
+
+            AMM amm(env, account, XRP(10'000), USD(10'000));
+            for (auto i = 0; i < maxDeletableAMMTrustLines + 10; ++i)
+            {
+                Account const a{std::to_string(i)};
+                env.fund(XRP(1'000), a);
+                env(trust(a, STAmount{amm.lptIssue(), 10'000}));
+                // set tsh collect
+                if (!testStrong)
+                    env(fset(a, asfTshCollect));
+                // set tsh hook
+                setTSHHook(env, a, testStrong);
+            }
+            amm.withdrawAll(account);
+            BEAST_EXPECT(amm.ammExists());
+
+            // set tsh collect
+            if (!testStrong)
+                addWeakTSH(env, issuer);
+
+            // set tsh hook
+            setTSHHook(env, issuer, testStrong);
+
+            // delete
+            amm.ammDelete(bob);
+
+            // verify tsh hook triggered
+            testTSHStrongWeak(env, tshNONE, __LINE__);
+        }
+    }
+
+    // AMMClawback
+    void
+    testAMMClawbackTSH(FeatureBitset features)
+    {
+        using namespace test::jtx;
+        using namespace std::literals;
+        testcase("amm clawback tsh");
+
+        // otxn: account
+        // tsh holder
+        // w/s: weak
+        for (bool const testStrong : {true, false})
+        {
+            test::jtx::Env env{
+                *this,
+                network::makeNetworkConfig(21337, "10", "1000000", "200000"),
+                features};
+
+            auto const issuer = Account("gw");
+            auto const account = Account("alice");
+            auto const USD = issuer["USD"];
+
+            env.fund(XRP(30'000), issuer, account);
+            env.close();
+            env(fset(issuer, asfAllowTrustLineClawback));
+            env.close();
+            env.trust(USD(30'000), account);
+            env.close();
+            env(pay(issuer, account, USD(30'000)));
+            env.close();
+
+            // create AMM
+            AMM ammAlice(env, account, XRP(10'000), USD(10'000));
+
+            // set tsh collect
+            if (!testStrong)
+                addWeakTSH(env, account);
+
+            // set tsh hook
+            setTSHHook(env, account, testStrong);
+
+            // clawback
+            env(amm::ammClawback(issuer, account, USD, XRP, USD(1000)));
+            env.close();
+
+            // verify tsh hook triggered
+            if (features[featureIOUIssuerWeakTSH])
+            {
+                auto const expected = testStrong ? tshNONE : tshWEAK;
+                testTSHStrongWeak(env, expected, __LINE__);
+            }
+            else
+            {
+                testTSHStrongWeak(env, tshNONE, __LINE__);
+            }
+        }
+    }
+
+    // AMMDeposit
+    void
+    testAMMDepositTSH(FeatureBitset features)
+    {
+        using namespace test::jtx;
+        using namespace std::literals;
+        testcase("amm deposit tsh");
+
+        // otxn: account
+        // tsh issuer
+        // w/s: weak
+        for (bool const testStrong : {true, false})
+        {
+            test::jtx::Env env{
+                *this,
+                network::makeNetworkConfig(21337, "10", "1000000", "200000"),
+                features};
+
+            auto const issuer = Account("gw");
+            auto const account = Account("alice");
+            auto const USD = issuer["USD"];
+
+            env.fund(XRP(30'000), issuer, account);
+            env.close();
+            env.trust(USD(30'000), account);
+            env.close();
+            env(pay(issuer, account, USD(30'000)));
+            env.close();
+
+            // create AMM
+            AMM ammAlice(env, account, XRP(10'000), USD(10'000));
+
+            // set tsh collect
+            if (!testStrong)
+                addWeakTSH(env, issuer);
+
+            // set tsh hook
+            setTSHHook(env, issuer, testStrong);
+
+            // deposit
+            ammAlice.deposit(account, 10);
+
+            // verify tsh hook triggered
+            if (features[featureIOUIssuerWeakTSH])
+            {
+                auto const expected = testStrong ? tshNONE : tshWEAK;
+                testTSHStrongWeak(env, expected, __LINE__);
+            }
+            else
+            {
+                testTSHStrongWeak(env, tshNONE, __LINE__);
+            }
+        }
+    }
+
+    // AMMVote
+    void
+    testAMMVoteTSH(FeatureBitset features)
+    {
+        using namespace test::jtx;
+        using namespace std::literals;
+        testcase("amm vote tsh");
+
+        // otxn: account
+        // tsh issuer
+        // w/s: none
+        for (bool const testStrong : {true, false})
+        {
+            test::jtx::Env env{
+                *this,
+                network::makeNetworkConfig(21337, "10", "1000000", "200000"),
+                features};
+
+            auto const issuer = Account("gw");
+            auto const account = Account("alice");
+            auto const USD = issuer["USD"];
+
+            env.fund(XRP(30'000), issuer, account);
+            env.close();
+            env.trust(USD(30'000), account);
+            env.close();
+            env(pay(issuer, account, USD(30'000)));
+            env.close();
+
+            // create AMM
+            AMM ammAlice(env, account, XRP(10'000), USD(10'000));
+
+            // set tsh collect
+            if (!testStrong)
+                addWeakTSH(env, issuer);
+
+            // set tsh hook
+            setTSHHook(env, issuer, testStrong);
+
+            // vote
+            ammAlice.vote(account, 100);
+
+            // verify tsh hook triggered
+            testTSHStrongWeak(env, tshNONE, __LINE__);
+        }
+    }
+
+    // AMMWithdraw
+    void
+    testAMMWithdrawTSH(FeatureBitset features)
+    {
+        using namespace test::jtx;
+        using namespace std::literals;
+        testcase("amm withdraw tsh");
+
+        // otxn: account
+        // tsh issuer
+        // w/s: weak
+        for (bool const testStrong : {true, false})
+        {
+            test::jtx::Env env{
+                *this,
+                network::makeNetworkConfig(21337, "10", "1000000", "200000"),
+                features};
+
+            auto const issuer = Account("gw");
+            auto const account = Account("alice");
+            auto const USD = issuer["USD"];
+
+            env.fund(XRP(30'000), issuer, account);
+            env.close();
+            env.trust(USD(30'000), account);
+            env.close();
+            env(pay(issuer, account, USD(30'000)));
+            env.close();
+
+            // create AMM
+            AMM ammAlice(env, account, XRP(10'000), USD(10'000));
+
+            // set tsh collect
+            if (!testStrong)
+                addWeakTSH(env, issuer);
+
+            // set tsh hook
+            setTSHHook(env, issuer, testStrong);
+
+            // withdraw
+            ammAlice.withdraw(account, 100);
+
+            // verify tsh hook triggered
+            if (features[featureIOUIssuerWeakTSH])
+            {
+                auto const expected = testStrong ? tshNONE : tshWEAK;
+                testTSHStrongWeak(env, expected, __LINE__);
+            }
+            else
+            {
+                testTSHStrongWeak(env, tshNONE, __LINE__);
+            }
         }
     }
 
@@ -6385,6 +6771,13 @@ private:
     {
         testAccountSetTSH(features);
         testAccountDeleteTSH(features);
+        testAMMBidTSH(features);
+        testAMMCreateTSH(features);
+        testAMMDeleteTSH(features);
+        testAMMClawbackTSH(features);
+        testAMMDepositTSH(features);
+        testAMMVoteTSH(features);
+        testAMMWithdrawTSH(features);
         testCheckCancelTSH(features);
         testCheckCashTSH(features);
         testCheckCreateTSH(features);
