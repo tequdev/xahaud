7eec903c94513e0e8535196750de7590f6896ac0
tequ <git@tequ.dev>
2025-08-04 21:19:38 +0900

    add conan export snapply,soci

--- build-core.sh
+++ build-core.sh
@@ -45,6 +45,8 @@ export CMAKE_STATIC_LINKER_FLAGS="-static-libstdc++"
 git config --global --add safe.directory /io &&
 git checkout src/ripple/protocol/impl/BuildInfo.cpp &&
 sed -i s/\"0.0.0\"/\"$(date +%Y).$(date +%-m).$(date +%-d)-$(git rev-parse --abbrev-ref HEAD)$(if [ -n "$4" ]; then echo "+$4"; fi)\"/g src/ripple/protocol/impl/BuildInfo.cpp &&
+conan export external/snappy snappy/1.1.9@ &&
+conan export external/soci soci/4.0.3@ &&
 cd release-build &&
 conan install .. --output-folder . --build missing --settings build_type=$BUILD_TYPE &&
 cmake .. -G Ninja -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_TOOLCHAIN_FILE:FILEPATH=build/generators/conan_toolchain.cmake -DLLVM_DIR=$LLVM_DIR -DWasmEdge_LIB=$WasmEdge_LIB -Dxrpld=TRUE -Dtests=TRUE &&
