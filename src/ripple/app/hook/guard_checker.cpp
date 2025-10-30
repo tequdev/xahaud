24e18c9766f3d3b1277b6d2868e7ebb210c47f6e
tequ <git@tequ.dev>
2025-10-30 22:18:10 +0900

    Refactor2: Hook APIs Amendment Guards

--- src/ripple/app/hook/guard_checker.cpp
+++ src/ripple/app/hook/guard_checker.cpp
@@ -1,3 +1,5 @@
+#define GUARD_CHECKER_BUILD
+#include "Enum.h"
 #include "Guard.h"
 #include <fcntl.h>
 #include <iostream>
@@ -79,7 +81,8 @@ main(int argc, char** argv)
 
     close(fd);
 
-    auto result = validateGuards(hook, std::cout, "", 3);
+    auto result =
+        validateGuards(hook, std::cout, "", hook_api::getImportWhitelist(), 3);
 
     if (!result)
     {
