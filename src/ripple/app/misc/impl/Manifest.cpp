6128817fc80a0d48b1da773bd6649d056553066f
tequ <git@tequ.dev>
2026-01-26 17:58:32 +0900

    Revert "fix: increment manifest sequence for client code cache invalidation (#631)"
    This reverts commit 775fb3a8b2084787e85e568b069d3b7e4b32cd33.

--- src/ripple/app/misc/impl/Manifest.cpp
+++ src/ripple/app/misc/impl/Manifest.cpp
@@ -471,10 +471,6 @@ ManifestCache::applyManifest(Manifest m)
 
         auto masterKey = m.masterKey;
         map_.emplace(std::move(masterKey), std::move(m));
-
-        // Increment sequence to invalidate cached manifest messages
-        seq_++;
-
         return ManifestDisposition::accepted;
     }
 
