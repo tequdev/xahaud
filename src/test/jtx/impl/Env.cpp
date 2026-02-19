959f7cd21ccd53d6161c64ac0074d2fc6bd12eed
Olek <115580134+oleks-rip@users.noreply.github.com>
2026-02-20 03:33:30 +0900

    Permissioned Domains (XLS-80d) (#5161)

--- src/test/jtx/impl/Env.cpp
+++ src/test/jtx/impl/Env.cpp
@@ -554,7 +554,8 @@ Env::autofill(JTx& jt)
     }
     catch (parse_error const&)
     {
-        test.log << "parse failed:\n" << pretty(jv) << std::endl;
+        if (!parseFailureExpected_)
+            test.log << "parse failed:\n" << pretty(jv) << std::endl;
         Rethrow();
     }
 }
