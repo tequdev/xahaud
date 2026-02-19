d12ae81070398c22df9743aaaa67b17d9a68d8bb
Ed Hennis <ed@ripple.com>
2026-02-19 18:49:44 +0900

    test: Env unit test RPC errors return a unique result: (#4877)
    * telENV_RPC_FAILED is a new code, reserved exclusively
      for unit tests when RPC fails. This will
      make those types of errors distinct and easier to test
      for when expected and/or diagnose when not.
    * Output RPC command result when result is not expected.

--- src/test/jtx/Env.h
+++ src/test/jtx/Env.h
@@ -535,7 +535,11 @@ public:
         of JTx submission.
     */
     void
-    postconditions(JTx const& jt, TER ter, bool didApply);
+    postconditions(
+        JTx const& jt,
+        TER ter,
+        bool didApply,
+        Json::Value const& jr = Json::Value());
 
     /** Apply funclets and submit. */
     /** @{ */
