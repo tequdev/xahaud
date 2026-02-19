6e51de48a197e592406b8d485540297754832713
Michael Legleux <legleux@users.noreply.github.com>
2026-02-19 20:44:23 +0900

    Add external directory to Conan recipe's exports (#5006)

--- conanfile.py
+++ conanfile.py
@@ -132,7 +132,7 @@ class Xrpl(ConanFile):
             self.requires('rocksdb/6.29.5')
 
     exports_sources = (
-        'CMakeLists.txt', 'Builds/*', 'bin/getRippledInfo', 'src/*', 'cfg/*'
+        'CMakeLists.txt', 'Builds/*', 'bin/getRippledInfo', 'src/*', 'cfg/*', 'external/*'
     )
 
     def layout(self):
